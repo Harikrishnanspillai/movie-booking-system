9876d8f10b9d02c8073d1f9a1eff8aba374e319d
Harikrishnanspillai <harikrishnansugesh@gmail.com>
2025-06-11 04:03:45 +0530

    dlt

--- MBS/src/Customer.java
+++
@@ -1,8 +0,0 @@
-public class Customer extends User{
-    public Customer (int userID, String name, String email, String passwd) {
-        this.userID = userID;
-        this.name = name;
-        this.email = email;
-        this.passwd = passwd;
-    }
-}
