b0bc71e4cdd558eb09d9f80b90cfc72562289578
Harikrishnanspillai <harikrishnansugesh@gmail.com>
2025-06-11 04:04:43 +0530

    Login + Admin panel

---
+++ MBS/src/Admin.java
@@ -0,0 +1,10 @@
+public class Admin extends User{
+
+    public Admin(int userID, String name, String email, String passwd) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.passwd = passwd;
+    }
+
+}
---
+++ MBS/src/App.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Panel Switching Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel title = new JLabel("Movie Ticket Booking System");
+        title.setFont(new Font("Courier New", Font.BOLD | Font.ITALIC, 22));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setVerticalAlignment(SwingConstants.CENTER);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        UserPanel userPanel = new UserPanel(mainPanel);
+        mainPanel.add(userPanel, BorderLayout.CENTER);
+        frame.add(title, BorderLayout.NORTH);
+        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+    }
+}
